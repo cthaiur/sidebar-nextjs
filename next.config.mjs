c8e62a8b790fa870bf576a954bed8aa7238a221e
Cristian Thaiur <thaiur@infly.net.br>
2024-07-26 10:57:08 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: { domains: ['assets.aceternity.com'], formats: ['image/avif', 'image/webp'], },
+};
 
 export default nextConfig;
